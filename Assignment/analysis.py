094c3b2bd3887d05b66c13e25267be6dbf18d589
pragma5501 <temo789@dgist.ac.kr>
2025-11-22 21:33:03 +0900

    Done : process implementation

--- Assignment/analysis.py
+++ Assignment/analysis.py
@@ -5,10 +5,11 @@ Pointer Analysis Analyzer
 Simplified analysis module that uses data structures from frontend.read_facts
 and statistics functions from util.stat
 """
-from typing import Set
 from dataclasses import dataclass
-from frontend.read_facts import InputFacts, find_main_method
 from itertools import product
+from typing import Set
+
+from frontend.read_facts import InputFacts, find_main_method
 
 
 def result_sum(varPtsTo, fldPtsTo, callGraph):
@@ -38,6 +39,21 @@ class FldPtsTo:
     heap: str
     field: str
     mappedHeap: str
+# | allocations            | Set of HeapAllocationFact – (variable, heap allocation, method) |
+# | alloc_types            | Set of HeapAllocTypeFact – (allocation site, allocated type)    |
+# | moves                  | Set of MoveFact – (to_variable, from_variable, method)          |
+# | loads                  | Set of LoadFact – (to_variable, from_variable, field, method)   |
+# | stores                 | Set of StoreFact – (base_variable, field, from_variable, method)|
+# | return_vars            | Set of ReturnVarFact – (variable, method)                       |
+# | virtual_invocations    | Set of VirtualInvocationFact – (invocation, base_variable, called_method_name, enclosing_method) |
+# | static_invocations     | Set of StaticInvocationFact – (invocation, called_method_signature, enclosing_method) |
+# | special_invocations    | Set of SpecialInvocationFact – (invocation, base_variable, called_method_signature, enclosing_method) |
+# | actual_params          | Set of ActualParamFact – (index, invocation, variable)          |
+# | formal_params          | Set of FormalParamFact – (index, method, variable)              |
+# | this_vars              | Set of ThisVarFact – (method, this_variable)                    |
+# | assign_return_values   | Set of AssignReturnValueFact – (invocation, assigned_variable)  |
+# | method_name_types      | Set of MethodNameTypeFact – (method, method_name, enclosing_class) |
+# | methods                | Set[str] – all method signatures
 
 
 class PointerAnalysisAnalyzer:
@@ -56,46 +72,147 @@ class PointerAnalysisAnalyzer:
 
     def process_alloc(self):
         # allocation = (variable, allocation_site, method)
-        # ToDo
-        pass
+        iter_item = [self.data.allocations, self._call_graph]
+        for alloc, cg in product(*iter_item):
+            conditions = [
+                alloc.method == cg.method,
+            ]
+            if all(conditions):
+                new_vp = VarPtsTo(alloc.variable, alloc.allocation_site)
+                self._var_points_to.add(new_vp)
 
     def process_move(self):
         # move = (to_variable, from_variable, method)
-        # ToDo
-        pass
+        new_vps = set()
+        iter_item = [self.data.moves, self._var_points_to, self._call_graph]
+        for move, vp, cg in product(*iter_item):
+            conditions = [
+                move.from_variable == vp.variable,
+                move.method == cg.method,
+            ]
+            if all(conditions):
+                new_vp = VarPtsTo(move.to_variable, vp.allocationSite)
+                self._var_points_to.add(new_vp)
 
     def process_store(self):
-        # store = (to_variable, field, from_variable, method)
-        # ToDo
-        pass
+        # store = (from_variable, to_variable, field, method)
+        iter_item = [self.data.stores, self._var_points_to, self._var_points_to, self._call_graph]
+        for store, vp_from, vp_base, cg in product(*iter_item):
+            conditions = [
+                store.to_variable == vp_base.variable,
+                store.from_variable == vp_from.variable,
+                store.method == cg.method,
+            ]
+            if all(conditions):
+                new_fp = FldPtsTo(
+                    vp_base.allocationSite, store.field, vp_from.allocationSite
+                )
+                self._fld_points_to.add(new_fp)
 
     def process_load(self):
         # load = (to_variable, from_variable, field, method)
-        # ToDo
-        pass
+        iter_item = [self.data.loads, self._var_points_to, self._fld_points_to, self._call_graph]
+        for load, vp, fp, cg in product(*iter_item):
+            conditions = [
+                load.from_variable == vp.variable,
+                load.field == fp.field,
+                load.method == cg.method,
+                vp.allocationSite == fp.heap
+            ]
+            if all(conditions):
+                new_vp = VarPtsTo(load.to_variable, fp.mappedHeap)
+                self._var_points_to.add(new_vp)
 
     def process_static_call(self):
         # static_call = (invocation, called_method_signature, enclosing_method)
-        # ToDo
-        pass
+
+        iter_item = [self.data.static_invocations, self._call_graph]
+        for static_call, cg in product(*iter_item):
+            conditions = [
+                static_call.enclosing_method == cg.method,
+            ]
+            if all(conditions):
+                new_cg = CallGraphEdge(
+                    static_call.invocation, static_call.called_method_signature
+                )
+                self._call_graph.add(new_cg)
 
     def process_special_call(self):
         # special_call = (invocation, called_method_signature, enclosing_method)
-        # ToDo
-        pass
+
+        iter_item = [
+            self._call_graph,
+            self.data.special_invocations,
+            self.data.this_vars,
+            self._var_points_to,
+        ]
+        for cg, special_call, this_var, vp in product(*iter_item):
+            conditions = [
+                special_call.base_variable == vp.variable,
+                special_call.called_method_signature == this_var.method,
+                special_call.enclosing_method == cg.method,
+            ]
+            if all(conditions):
+                new_cg = CallGraphEdge(
+                    special_call.invocation, special_call.called_method_signature
+                )
+                new_vp = VarPtsTo(this_var.variable, vp.allocationSite)
+                self._call_graph.add(new_cg)
+                self._var_points_to.add(new_vp)
 
     def process_virtual_call(self):
         # virtual_call = (invocation, called_method_tmp, enclosing_method)
-        # ToDo
-        pass
+        iter_item = [
+            self.data.virtual_invocations,
+            self._call_graph,
+            self._var_points_to,
+            self.data.alloc_types,
+            self.data.method_name_types,
+            self.data.this_vars,
+        ]
+        for virtual_call, cg, vp, alloc_type, method_name_type, this_var in product(
+            *iter_item
+        ):
+            conditions = [
+                virtual_call.base_variable == vp.variable,
+                virtual_call.called_method_name == method_name_type.method_name,
+                virtual_call.enclosing_method == cg.method,
+                vp.allocationSite == alloc_type.allocation_site,
+                alloc_type.allocated_type == method_name_type.enclosing_class,
+                method_name_type.method == this_var.method,
+            ]
+            if all(conditions):
+                new_cg = CallGraphEdge(
+                    virtual_call.invocation, method_name_type.method
+                )
+                new_vp = VarPtsTo(this_var.variable, vp.allocationSite)
+                self._call_graph.add(new_cg)
+                self._var_points_to.add(new_vp)
 
     def process_param(self):
-        # ToDo
-        pass
+        iter_item = [self._call_graph, self.data.actual_params, self.data.formal_params, self._var_points_to]
+        for cg, actual_param, formal_param, vp in product(*iter_item):
+            conditions = [
+                actual_param.index == formal_param.index,
+                actual_param.variable == vp.variable,
+                actual_param.invocation == cg.invocationSite,
+                formal_param.method == cg.method,
+            ]
+            if all(conditions):
+                new_vp = VarPtsTo(formal_param.variable, vp.allocationSite)
+                self._var_points_to.add(new_vp)
 
     def process_return(self):
-        # ToDo
-        pass
+        iter_item = [self._call_graph, self.data.assign_return_values, self.data.return_vars, self._var_points_to]
+        for cg, assign_return_var, return_var, vp in product(*iter_item):
+            conditions = [
+                cg.invocationSite == assign_return_var.invocation,
+                cg.method == return_var.method,
+                return_var.variable == vp.variable,
+            ]
+            if all(conditions):
+                new_vp = VarPtsTo(assign_return_var.variable, vp.allocationSite)
+                self._var_points_to.add(new_vp)
 
     def analysis(self):
         """Run the pointer analysis algorithm"""
@@ -120,7 +237,7 @@ class PointerAnalysisAnalyzer:
             self.process_virtual_call()
             self.process_param()
             self.process_return()
-            
+
             current_results = self.results_count()
             changed = current_results > previous_results
 
